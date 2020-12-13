d96ee07a42fa4e0ad3286c05232778fb5f881d17
David Arroyo Menéndez <davidam@gmail.com>
2020-12-13 19:26:37 +0100

    app/character.py: add nextRound method

--- practica1/app/character.py
+++ practica1/app/character.py
@@ -6,6 +6,10 @@ class Character(Root):
     def __init__(self, hp, dmg):
         super().__init__(hp, dmg)
         self.victory = False
+        self.rounds_accumulated = 0
+
+    def nextRound(self):
+        self.rounds_accumulated = self.rounds_accumulated + 1
         
     def is_alive(self):
         if self.hp > 0:
