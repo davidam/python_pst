1daeb6555ecef6c20bc6e8300c8acca029a8d563
David Arroyo Menéndez <davidam@gmail.com>
2020-12-20 18:43:19 +0100

    app/whatsapper.py: add treatment

--- practica1/app/whatsapper.py
+++ practica1/app/whatsapper.py
@@ -8,5 +8,8 @@ class Whatsapper(Character):
         super().__init__(20, 6)
         self.victory = False
         
-
-
+    def treatment(self, player):
+        value = 2 * self.dmg
+        if ((self.turn / 3) == 0):
+            player.hp = player.hp + value
+        return player.hp
