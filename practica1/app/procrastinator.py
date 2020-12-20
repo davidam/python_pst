e6f5752d2f51a4cf4743ef5b061b8a2246a7d6db
David Arroyo Menéndez <davidam@gmail.com>
2020-12-20 18:30:56 +0100

    app/procrastinator.py: taking into account special rule

--- practica1/app/procrastinator.py
+++ practica1/app/procrastinator.py
@@ -7,4 +7,7 @@ class Procrastinator(Character):
     def __init__(self):
         super().__init__(30, 6)
         
+    def hurt(self):
+        value = super.hurt() + (self.round - 1)
+        return value
 
